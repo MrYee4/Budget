40ead3423aee3c223fe6e35c8c1dc646975c23ac
Spencer <spencer.nettles2134@gmail.com>
2020-10-04 00:36:44 -0600

    made table

--- budget.py
+++ budget.py
@@ -10,13 +10,27 @@ class Budget:
         self.rent = rent
         self.food = food
         self.gas = gas
+        self.fun = fun
 
     def printBudget(self):
-        print("")
+        print("       |   Weekly   |   Monthly   |")
+        print("-------|------------|-------------|")
+        print("Income |", " "*(9-len(str(self.income))), self.income, "|", " "*(10-len(str(self.mIncome))), self.mIncome, "|")
+        print("-------|------------|-------------|")
+        print("Rent   |", " "*(9-len(str(self.rent))), self.rent, "|", " "*(10-len(str(self.rent * 4))), self.rent * 4, "|")
+        print("Food   |", " "*(9-len(str(self.food))), self.food, "|", " "*(10-len(str(self.food * 4))), self.food * 4, "|")
+        print("Gas    |", " "*(9-len(str(self.gas))), self.gas, "|", " "*(10-len(str(self.gas * 4))), self.gas * 4, "|")
+        print("Fun    |", " "*(9-len(str(self.fun))), self.fun, "|", " "*(10-len(str(self.fun * 4))), self.fun * 4, "|")
+        print("-------|------------|-------------|")
+        print("Savings|", " "*(9-len(str(self.calculateSaving()))), self.calculateSaving(), "|", " "*(10-len(str(self.calculateSaving() * 4))), (self.calculateSaving() * 4), "|")
 
     def printIncome(self):
         print("weekly income: ", self.income)
         print("Monthly income: ", self.mIncome)
+
+    def calculateSaving(self):
+        self.saving = self.income - (self.rent + self.food + self.gas + self.fun)
+        return self.saving
 
 
 
@@ -33,6 +47,8 @@ def main():
 
     b1 = Budget(income, rent, food, gas, fun)
     b1.printIncome()
+    print("")
+    b1.printBudget()
 
 
 if __name__ == "__main__":
