d4bd208556fa0d774e521cafc6ca9f3f6172f49f
Spencer <spencer.nettles2134@gmail.com>
2020-10-03 23:46:10 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Budget
+## This program will test my knowledge of classes
+
+This will take in my budget and expenses and display a budget for me
+It will also calculate my weekly and monthly income
---
+++ budget.py
@@ -0,0 +1,14 @@
+# Spencer Nettles
+# CS240
+# Budget will take in my income and expenses and diplay my monthly budget
+
+
+
+
+
+def main():
+    print("Hello")
+
+
+if __name__ == "__main__":
+    main()
