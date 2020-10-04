499c619fa2ee828921c236476ff95e1e10121947
Spencer <spencer.nettles2134@gmail.com>
2020-10-04 01:24:26 -0600

    put inputs in class & allows mulitple budgets

--- budget.py
+++ budget.py
@@ -5,13 +5,27 @@
 #This budget class will contain all the info needed for the budget
 class Budget:
     #class constructor
-    def __init__(self, income, rent, food, gas, fun):
-        self.income = income
-        self.mIncome = 4 * income
-        self.rent = rent
-        self.food = food
-        self.gas = gas
-        self.fun = fun
+    def __init__(self):
+        self.pay = 0
+        self.income = 0
+        self.mIncome = 0
+        self.hours = 0
+        self.rent = 0
+        self.food = 0
+        self.gas = 0
+        self.fun = 0
+
+    #asks user for budget data
+    def getExpense(self):
+        self.pay = float(input("How much do you make an hour: "))
+        self.hours = float(input("How many hours do you work a week: "))
+        self.income = (self.pay * self.hours)
+        self.mIncome = self.income * 4
+        self.rent = float(input("How much is rent per month: "))
+        self.rent /= 4
+        self.food = float(input("How much do you spend on food per week: "))
+        self.gas = float(input("How much is gas per week: "))
+        self.fun = float(input("How much do you want to spend for fun a week: "))
 
     #Will print out budget in a table
     def printBudget(self):
@@ -24,12 +38,12 @@ class Budget:
         print("Gas    |", " "*(9-len(str(self.gas))), self.gas, "|", " "*(10-len(str(self.gas * 4))), self.gas * 4, "|")
         print("Fun    |", " "*(9-len(str(self.fun))), self.fun, "|", " "*(10-len(str(self.fun * 4))), self.fun * 4, "|")
         print("-------|------------|-------------|")
-        print("Savings|", " "*(9-len(str(self.calculateSaving()))), self.calculateSaving(), "|", " "*(10-len(str(self.calculateSaving() * 4))), (self.calculateSaving() * 4), "|")
+        print("Savings|", " "*(9-len(str(self.calculateSaving()))), self.calculateSaving(), "|", " "*(10-len(str(self.calculateSaving() * 4))), (self.calculateSaving() * 4), "|\n")
 
     #displays income to user
     def printIncome(self):
         print("weekly income: ", self.income)
-        print("Monthly income: ", self.mIncome)
+        print("Monthly income: ", self.mIncome, "\n")
 
     #calculates how much money is left over after expenses
     def calculateSaving(self):
@@ -40,21 +54,18 @@ class Budget:
 
 
 def main():
-    #asks user for budget values
-    pay = float(input("How much do you make an hour: "))
-    hours = float(input("How many hours do you work a week: "))
-    income = (pay * hours)
-    rent = float(input("How much is rent per month: "))
-    rent /= 4
-    food = float(input("How much do you spend on food per week: "))
-    gas = float(input("How much is gas per week: "))
-    fun = float(input("How much do you want to spend for fun a week: "))
-
-    #budget object
-    b1 = Budget(income, rent, food, gas, fun)
+    b1 = Budget()
+    b1.getExpense()
     b1.printIncome()
-    print("")
     b1.printBudget()
+
+    #asks if the user wants to make another budget to allow him to make as many as he needs
+    again = input("Do you want to make another bugdet(y/n): ")
+    while again == "y":
+        b1.getExpense()
+        b1.printIncome()
+        b1.printBudget()
+        again = input("Do you want to make another bugdet(y/n): ")
 
 
 if __name__ == "__main__":
