14c7c4426c72b45974e704f85ecfe31fda5e390f
Spencer <spencer.nettles2134@gmail.com>
2020-10-03 23:46:26 -0600

    init

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 ## This program will test my knowledge of classes
 
 This will take in my budget and expenses and display a budget for me
-It will also calculate my weekly and monthly income
+It will also calculate and monthly income and tell me if I am sepending too much
--- budget.py
+++ budget.py
@@ -3,11 +3,24 @@
 # Budget will take in my income and expenses and diplay my monthly budget
 
 
+class Budget:
+    def __init__(self, income, rent, food, gas, fun):
+        self.income = income
+        self.rent = rent
+        self.food = food
+        self.gas = gas
+
+    def printBudget(self):
+        print("")
+
 
 
 
 def main():
-    print("Hello")
+    pay = float(input("How much do you make an hour: "))
+    hours = float(input("How many hours do you work a week: "))
+    
+    Budget b1()
 
 
 if __name__ == "__main__":
